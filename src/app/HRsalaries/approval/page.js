1c1488b12dc9545507d9ed1c120e11a644eb8693
itzMeTiru <Tishanyasiru@outlook.com>
2025-03-17 23:50:05 +0530

    created salary section

---
+++ src/app/HRsalaries/approval/page.js
@@ -0,0 +1,8 @@
+export default function GetApproval() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-xl font-bold">Get Approval</h1>
+      </div>
+    );
+  }
+  
